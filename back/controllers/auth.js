8aabc7d2446a84f82660f65970d032ce841aa316
JoseRabal <jrabal.segura@gmail.com>
2024-04-01 16:42:35 +0200

    Añado checks en rutas como middleware

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -1,35 +1,44 @@
-
+const { validationResult } = require('express-validator');
 
 const createEmployee = (req, res) => {
 
-  const {name, email, password} = req.body;
+	const {name, email, password} = req.body;
 
-  res.status(201).json({
-      "ok": true,
-      "message": "New user created",
-      name,
-      email,
-      password
-  });
+  	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			"ok": false,
+			"errors": errors.mapped()
+		});
+	}
+
+	res.status(201).json({
+		"ok": true,
+		"message": "New user created",
+		name,
+		email,
+		password
+	});
 }
 
 const loginEmployee = (req, res) => {
 
-  const {email, password} = req.body;
+	const {email, password} = req.body;
 
-  res.json({
-      "ok": true,
-      "message": "Login",
-      email,
-      password
-  });
+	res.json({
+		"ok": true,
+		"message": "Login",
+		email,
+		password
+	});
 }
 
 const renewToken = (req, res) => {
-  res.json({
-      "ok": true,
-      "message": "Token renewed"
-  });
+	res.json({
+		"ok": true,
+		"message": "Token renewed"
+	});
 }
 
 module.exports = {
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -5,13 +5,28 @@
 
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 const { createEmployee, loginEmployee, renewToken } = require('../controllers/auth');
 
 
-router.post('/new', createEmployee);
+router.post(
+    '/new', 
+    [
+        check('name', 'The name is required').not().isEmpty(),
+        check('email', 'The email is required').isEmail(),
+        check('password', 'The password is required').not().isEmpty(),
+        check('password', 'The password must be at least 6 characters').isLength({ min: 6 })
+    ], 
+    createEmployee);
 
-router.post('/', loginEmployee);
+router.post(
+    '/',
+    [
+        check('email', 'The email is required').isEmail(),
+        check('password', 'The password is required').not().isEmpty()
+    ],
+    loginEmployee);
 
 router.get('/renew', renewToken);
 
