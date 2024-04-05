e9d422e9728a15541784801f5812c475abfc1fac
JoseRabal <jrabal.segura@gmail.com>
2024-04-05 17:27:25 +0200

    Fixed some bugs in croner

--- back/cronjobs/cronjobs.js
+++ back/cronjobs/cronjobs.js
@@ -7,7 +7,7 @@ const {clearHoursAndHolidays} = require('./clearHoursAndHolidays');
 const {checkMandatoryRest} = require('./checkMandatoryRest');
 
 //Check holidays every day at 07:00, check if one start or end today
-Cron('0 7 * * *', () => checkHolidays, {timeZone: 'Europe/Madrid'});
+Cron('0 7 * * *', () => checkHolidays);
 
 // Run after the first hour every shift, get the shifts programmatically:
 const startShifts = Object.values(shifts).map(shift => shift.start);
@@ -15,17 +15,21 @@ const startShiftsNumbers = startShifts.map(shift => shift.split(':')[0]);
 
 startShiftsNumbers.forEach(shiftStartHour => {
     // Schedule a job for 30 minutes after the start of each shift
-    Cron(`30 ${shiftStartHour} * * *`, () => checkAsistencia, {timeZone: 'Europe/Madrid'});
+    Cron(`30 ${shiftStartHour} * * *`, () => checkAsistencia);
   }
 );
 
 // Run once a day at 07:00 ahd deletePastShifts
-Cron('0 7 * * *', () => deletePastShifts, {timeZone: 'Europe/Madrid'});
+Cron('0 7 * * *', () => deletePastShifts);
 
 // Run once a year, the 1st of january at 07:00
-Cron('0 7 1 1 *', () => clearHoursAndHolidays, {timeZone: 'Europe/Madrid'});
+Cron('0 7 1 1 *', () => clearHoursAndHolidays);
 
 // Run once a week, Monday at 07:00
-Cron('0 7 * * 1', () => checkMandatoryRest, {timeZone: 'Europe/Madrid'});
+Cron('0 7 * * 1', () => checkMandatoryRest);
+
+//Trial
+//Cron('* * * * *', () => console.log('Server is alive'));
+//Cron('20 17 * * *', () => console.log('Server is alive, 17:20'));
 
 
