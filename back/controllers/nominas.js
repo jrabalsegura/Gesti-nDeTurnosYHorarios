de3a8b9f818663c6d8fff692bc95386bbcbac101
JoseRabal <jrabal.segura@gmail.com>
2024-04-30 10:23:57 +0200

    Not allow several nominas for employee y mes x2

--- back/controllers/nominas.js
+++ back/controllers/nominas.js
@@ -19,13 +19,14 @@ const createNomina = async (req, res) => {
     const nominas = await Nomina.find({employeeId, month, year});
     if (nominas.length > 0) {
         res.status(500).json({ok: false, msg: 'Nomina ya existe para empleado y mes'});
+        return;
     }
     try {
         const newNomina = new Nomina({employeeId, month, year, baseSallary, horasExtra, socialSecurity, pago});
         await newNomina.save();
         res.status(200).json({ok: true, nomina});
     } catch (error) {
-        res.status(500).json({ok: false, msg: 'Error creating nomina'});
+        res.status(500).json({ok: false, msg: 'Error creating nomina'}, error);
     }
 }
 
