5b7ce222a46dfcf6ba812f40b88066278309a6c0
JoseRabal <jrabal.segura@gmail.com>
2024-05-01 13:32:50 +0200

    Configuring upload endpoint to work with aws s3 bucket fixingx4

--- back/aws/config.js
+++ back/aws/config.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-const multer = require('multer');
+
 
 // Configure AWS to use your credentials
 AWS.config.update({
--- back/routes/upload.js
+++ back/routes/upload.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {validateJWT} = require('../middlewares/validate-JWT');
 const {uploadFile} = require('../controllers/upload');
+const multer = require('multer');
+
+// Configure multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.use(validateJWT);
 
-router.post('/', uploadFile);
+router.post('/', upload.single('file'), uploadFile);
 
 module.exports = router;
 
