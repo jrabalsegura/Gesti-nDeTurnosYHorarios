c901d4e9a4ad0bccb9b8694e0145a7c9327768ee
JoseRabal <jrabal.segura@gmail.com>
2024-05-01 14:17:13 +0200

    Configuring upload endpoint to work with aws s3 bucket debuggin

--- back/controllers/upload.js
+++ back/controllers/upload.js
@@ -8,13 +8,18 @@ const uploadFile = async (req, res) => {
     // accessing the file
     console.log(req.files.file)
     const name = req.files.file.name
-    const fileContent = Buffer.from(req.files.file.data)
+    const fileContent = req.files.file.data
+
+    console.log('Original data type:', fileContent.constructor.name); // Log the original data type
+
+    const buffer = Buffer.from(fileContent); // Convert to Buffer
+    console.log('Converted data type:', buffer.constructor.name); 
 
     let fileUrl = '';
 
     // Upload to S3
     try {
-        fileUrl = await uploadSelectedFile(name, fileContent);
+        fileUrl = await uploadSelectedFile(name, buffer);
         console.log('File URL:', fileUrl);
     } catch (err) {
         console.error(err);
