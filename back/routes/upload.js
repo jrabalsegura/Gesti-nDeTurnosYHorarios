c679fa2b34db1c0fd044eabcf19a8ecd5f3ff99b
JoseRabal <jrabal.segura@gmail.com>
2024-05-01 14:02:54 +0200

    Configuring upload endpoint to work with aws s3 bucket new aproach using fileUploadx3

--- back/routes/upload.js
+++ back/routes/upload.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {validateJWT} = require('../middlewares/validate-JWT');
 const {uploadFile} = require('../controllers/upload');
-const fileUpload = require('express-fileupload');
 
-//router.use(validateJWT);
-
-router.use(fileUpload())
+router.use(validateJWT);
 
 router.post('/', uploadFile);
 
