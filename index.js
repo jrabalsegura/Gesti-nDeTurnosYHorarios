bd4083b23744c5078fbc83f04e7a2fdf25bc029c
JoseRabal <jrabal.segura@gmail.com>
2024-05-20 17:20:57 +0200

    Use env variables for access

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { default: mongoose } = require('mongoose');
 const app = express();
 
 // CORS
-const allowedOrigins = ['http://localhost:5173', 'https://gestion-horarios-cd0d24b996c6.herokuapp.com', 'http://gestionhorarios.eu-west-1.elasticbeanstalk.com'];
+const allowedOrigins = ['https://gestion-horarios-cd0d24b996c6.herokuapp.com', 'http://gestionhorarios.eu-west-1.elasticbeanstalk.com'];
 
 app.use(cors({
     origin: allowedOrigins
@@ -39,13 +39,17 @@ app.use('/employees', require('./routes/employees'));
 app.use('/upload', require('./routes/upload'));
 app.use('/download', require('./routes/download'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+  });
+
 let server;
 
 async function startApp() {
     await dbConnection(); // Ensure DB connection is ready
     server = app.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
-        createUser({name: "admin", username: "admin@admin.com", password: "12345678"}); // Consider moving this out if it doesn't need to run every time
+        createUser({name: "admin", username: process.env.ADMIN_EMAIL, password: process.env.ADMIN_PASSWORD}); 
     });
     return server;
 }
